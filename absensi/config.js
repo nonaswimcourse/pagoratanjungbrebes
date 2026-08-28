918413dd7878f36cf3c39562293ab74886b0e070
nona swimming course <keishaayundiainara27@gmail.com>
2026-08-29 01:29:24 +0700

    Export Supabase URL and anon key from config.js

--- absensi/config.js
+++ absensi/config.js
@@ -1,4 +1,4 @@
 // Isi dengan URL dan anon/publishable key project Supabase Anda.
 // Jangan gunakan service_role key di browser.
-const SUPABASE_URL = 'https://bixyaowckwvjpgwffoci.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_phZErDKE6oDEDN5whvlk3Q_8LpXylcG';
+export const SUPABASE_URL = "https://bixyaowckwvjpgwffoci.supabase.co";
+export const SUPABASE_ANON_KEY = "sb_publishable_phZErDKE6oDEDN5whvlk3Q_8LpXylcG";
